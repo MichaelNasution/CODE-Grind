40cae26ef126cac8504a31b9465ae5233b3cb892
Michael Pratama Nasution <nasutionkale@gmail.com>
2026-05-11 08:31:18 +0700

    refactor analysis concept in main.py

--- NBA/main.py
+++ NBA/main.py
@@ -1,13 +1,18 @@
 import requests
 import pandas as pd
 
+from analyze import analyze_games
+
 url = "https://api.balldontlie.io/v1/games"
 
 headers = {
     "Authorization": "658225bf-983a-4ab6-ad78-a52a787d8afe"
 }
 
-response = requests.get(url, headers=headers)
+response = requests.get(
+    url,
+    headers=headers
+)
 
 data = response.json()
 
@@ -15,4 +20,14 @@ games = data["data"]
 
 df = pd.DataFrame(games)
 
-print(df.head())
+# ANALYSIS
+analyzed_df = analyze_games(df)
+
+print(analyzed_df[[
+    "home_team_name",
+    "visitor_team_name",
+    "home_team_score",
+    "visitor_team_score",
+    "total_points",
+    "predicted_winner"
+]].head())
